0a1ab11d0b1ce75534fcb59360bf8c0af37c3e15
DIEGO <diegobest55@outlook.com>
2023-09-20 02:05:58 -0300

    projeto pagina do celular s23

---
+++ vida.js
@@ -0,0 +1,12 @@
+function trocaCor(cor){
+
+ let circulo = document.querySelector(".circulo")
+ circulo.style.background  = cor
+}
+
+
+function trocaimg(img){
+    let imagem = document.querySelector(".img")
+    imagem.src = img
+
+}
